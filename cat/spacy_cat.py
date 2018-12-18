Add  disambiguation with vectors, plus coomatrix, plus acc calculation. And some smaller fixes

cat/spacy_cat.py
@@ -1,76 +1,108 @@
 from spacy.tokens import Span
+from cat.cat_ann import CatAnn
+import numpy as np
 import operator
+from gensim.matutils import unitvec
+
+CNTX_SPAN = 6
+NUM = "NUMNUM"
 
 class SpacyCat(object):
-    def __init__(self, umls, vocab=None):
+    """
+
+    """
+    def __init__(self, umls, vocab=None, train=False, adv_disambig=False):
         self.umls = umls
         self.vocab = vocab
+        self.train = train
+        self.adv_disambig = adv_disambig
+        self.cat_ann = CatAnn(self.umls, self._add_ann)
+
         if self.vocab is None:
             self.vocab = self.umls.vocab
 
-    def _add_ann(self, cui, doc, tkns):
+
+    def _calc_acc(self, cui, doc, tkns):
+        start = max(0, tkns[0].i - CNTX_SPAN)
+        stop = min(len(doc), tkns[-1].i + CNTX_SPAN + 1)
+        # Add cntx words
+        self.umls.add_context_words(cui, self.doc_words[start:stop])
+
+        cntx = None
+        cnt = 0
+        for word in self.doc_words[start:stop]:
+            if word.isnumeric() or word == 'numnum':
+                word = NUM
+            elif word not in self.vocab:
+                # Skip if word is not in vocab
+                continue
+
+            if cntx is not None:
+                cntx = cntx + self.vocab.vec(word)
+            else:
+                cntx = self.vocab.vec(word)
+
+            cnt += 1
+
+        if cui in self.umls.cui2context_vec and cnt > 4:
+            print(self.doc_words[start:stop])
+            return np.dot(unitvec(cntx), unitvec(self.umls.cui2context_vec[cui]))
+        else:
+            return -1
+
+    def _add_cntx_vec(self, cui, doc, tkns):
+        start = max(0, tkns[0].i - CNTX_SPAN)
+        stop = min(len(doc), tkns[-1].i + CNTX_SPAN + 1)
+        # Add cntx words
+        self.umls.add_context_words(cui, self.doc_words[start:stop])
+
+        cntx_vecs = []
+        for word in self.doc_words[start:stop]:
+            if word.isnumeric() or word == 'numnum':
+                word = NUM
+            elif word not in self.vocab or self.vocab.vec(word) is None:
+                # Skip if word is not in vocab or no vector present
+                continue
+            cntx_vecs.append(self.vocab.vec(word))
+
+        cntx = np.average(cntx_vecs, axis=0)
+        if cui in self.umls.cui2context_vec and len(cntx_vecs) > 4:
+            if np.dot(unitvec(cntx), unitvec(self.umls.cui2context_vec[cui])) < 0.01:
+                print("SIMILARITY::::::::::::::::::::")
+                print(self.doc_words[start:stop])
+                print(cui)
+                print(tkns)
+                print(np.dot(unitvec(cntx), unitvec(self.umls.cui2context_vec[cui])))
+                print("UUUUUUUUUUUUUUUUUUUUUUUUUUUUUU\n")
+
+        if len(cntx_vecs) > 4:
+            self.umls.add_context_vec(cui, cntx)
+
+            # Add negative words, try with 2 for now
+            negs = self.vocab.get_negative_samples(n=1)
+            neg_cntx_vecs = [self.vocab.vec(self.vocab.index2word[x]) for x in negs]
+            neg_cntx = np.average(neg_cntx_vecs, axis=0)
+            self.umls.add_context_vec(cui, neg_cntx, negative=True)
+
+
+    def _add_ann(self, cui, doc, tkns, acc=-1):
         lbl = doc.vocab.strings.add(cui)
         ent = Span(doc, tkns[0].i, tkns[-1].i + 1, label=lbl)
+        if acc == -1:
+            # If accuracy was not calculated, do it now
+            acc = self._calc_acc(cui, doc, tkns)
+        ent._.acc = acc
         doc._.ents.append(ent)
 
-
-    def _scores_words(self, name, doc):
-        scores = {}
-        doc_words = [x._.norm for x in doc]
-
-        for cui in self.umls.name2cui[name]:
-            score = 0
-            n = 0
-            for word in self.umls.cui2words[cui].keys():
-                if word in doc_words:
-                    n += 1
-                    score += self.umls.cui2words[cui][word] / self.umls.vocab[word]
-            if n > 0:
-                score = score / n
-            scores[cui] = score
-        return scores
-
-    def _n_words_appearing(self, name, doc):
-        cui = list(self.umls.name2cui[name])[0]
-        doc_words = [x._.norm for x in doc]
-
-        n = 0
-        for word in self.umls.cui2words[cui].keys():
-            n += doc_words.count(word)
-
-        return n
-
-
-
-
-    def add_ann(self, name, tkns, doc, to_disamb):
-        one_tkn_upper = False
-        if len(tkns) == 1 and tkns[0].is_upper:
-            one_tkn_upper = True
-
-        if len(name) < 5 and len(tkns) > 1:
-            # Don't allow concatenation if len(name) < 5
-            pass
-        elif len(self.umls.name2cui[name]) == 1 and (len(name) > 3 or one_tkn_upper):
-            #TODO: can't be only one tkn upper
-            if len(name) > 6 or one_tkn_upper:
-                cui = list(self.umls.name2cui[name])[0]
-                self._add_ann(cui, doc, tkns)
-            else:
-                n_words = self._n_words_appearing(name, doc)
-                if n_words > 2:
-                    cui = list(self.umls.name2cui[name])[0]
-                    self._add_ann(cui, doc, tkns)
-        elif len(self.umls.name2cui[name]) > 1 and len(name) > 5:
-            # Probably everything is fine, just multiple concepts with exact name
-            scores = self._scores_words(name, doc)
-            cui = max(scores.items(), key=operator.itemgetter(1))[0]
-            self._add_ann(cui, doc, tkns)
+        # Increase cui count for this one
+        if cui in self.umls.cui_count:
+            self.umls.cui_count[cui] += 1
         else:
-            #print(name)
-            #print(self.umls.name2cui[name])
-            #print("____________")
-            to_disamb.append((tkns, name))
+            self.umls.cui_count[cui] = 1
+
+        if self.train:
+            self._add_cntx_vec(cui, doc, tkns)
+            self._cuis.append(cui)
 
 
     def create_main_ann(self, doc):
@@ -93,7 +125,10 @@ class SpacyCat(object):
 
 
     def __call__(self, doc):
+        self._cuis = []
         doc._.ents = []
+        doc_words = [t._.norm if not t._.is_punct else "PUCT" for t in doc]
+        self.doc_words = [t if not t.isnumeric() else NUM for t in doc_words]
 
         _doc = []
         for token in doc:
@@ -108,7 +143,7 @@ class SpacyCat(object):
 
             if name in self.umls.name2cui:
                 # Add annotation
-                self.add_ann(name, tkns, doc, to_disamb)
+                self.cat_ann.add_ann(name, tkns, doc, to_disamb, doc_words)
 
             for j in range(i+1, len(_doc)):
                 name = name + _doc[j]._.norm
@@ -119,11 +154,36 @@ class SpacyCat(object):
                     break
                 else:
                     if name in self.umls.name2cui:
-                        self.add_ann(name, tkns, doc, to_disamb)
+                        self.cat_ann.add_ann(name, tkns, doc, to_disamb, doc_words)
 
         self.create_main_ann(doc)
 
         #TODO: Disambiguate
-        print(len(to_disamb))
+        if True or self.adv_disambig:
+            print(len(to_disamb))
+            print(to_disamb)
+            for t in to_disamb:
+                tkns = t[0]
+                name = t[1]
+                tr = [tr.lower_ for tr in tkns]
+                if "po" in list(tr):
+                    continue 
+
+                cuis = self.umls.name2cui[name]
+
+                for cui in cuis:
+                    acc = self._calc_acc(cui, tkns[0].doc, tkns)
+                    # Add only if acc > 0 and not training, we can't have train and adv_disambig
+                    if acc > 0 and not self.train:
+                        self._add_ann(cui, tkns[0].doc, tkns, acc)
+                    print("&"*100)
+                    print(cui)
+                    print(name)
+                    print(tkns)
+                    print(acc)
+
+        # Add coocurances always
+        self.umls.add_coos(self._cuis)
 
         return doc
+