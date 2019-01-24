977d47fc31ad0b3ded0f89553e6d8eb75c4a46f6
w-is-h <w.kraljevic@gmail.com>
2019-01-24 19:19:37 +0000

    Removed the test files

--- test/prepare_w2v_data.py
+++
@@ -1,6 +0,0 @@
-from preprocessing.iterators import EmbMimicCSV
-from preprocessing.prep_for_embedding import clean_and_phrase
-
-csv_paths = ["/home/ubuntu/data/mimic/raw/test.csv"]
-
-iter_data = EmbMimicCSV
