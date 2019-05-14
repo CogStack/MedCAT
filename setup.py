d7874f210ee31433d75b69fe3a22091f20846c64
w-is-h <w.kraljevic@gmail.com>
2019-05-14 08:58:16 +0000

    Fix for cat -> medcat rename

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("./README.md", "r") as fh:
 
 setuptools.setup(
     name="medcat",
-    version="0.1.8",
+    version="0.1.9.2",
     author="w-is-h",
     author_email="w.kraljevic@gmail.com",
     description="Concept annotation tool for Electronic Health Records",
