68a344dc30ca65d24e9e42a28ea1d074203aaac9
w-is-h <w.kraljevic@gmail.com>
2019-05-23 08:53:26 +0000

    Requirements updated

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("./README.md", "r") as fh:
 
 setuptools.setup(
     name="medcat",
-    version="0.1.9.2",
+    version="0.1.9.3",
     author="w-is-h",
     author_email="w.kraljevic@gmail.com",
     description="Concept annotation tool for Electronic Health Records",
@@ -14,11 +14,11 @@ setuptools.setup(
     url="https://github.com/w-is-h/cat",
     packages=['medcat', 'medcat.utils', 'medcat.preprocessing'],
     install_requires=[
-        'numpy==1.15.4',
-        'pandas==0.23.4',
-        'gensim==3.7.1',
-        'spacy==2.1.3',
-        'scipy==1.1.0',
+        'numpy',
+        'pandas',
+        'gensim',
+        'spacy',
+        'scipy',
         'scispacy==0.2.0',],
     classifiers=[
         "Programming Language :: Python :: 3",
