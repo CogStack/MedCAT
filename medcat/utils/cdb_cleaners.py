b93e71ac37edbe74c59c0a3a6613a665d341fa9f
w-is-h <w.kraljevic@gmail.com>
2019-08-28 14:49:08 +0000

    fixed a bug in cleaners for cdb

--- medcat/utils/cdb_cleaners.py
+++ medcat/utils/cdb_cleaners.py
@@ -4,5 +4,6 @@ def clean_primary_name(cdb):
 
     for cui in cdb.cui2pref_name.keys():
         name = cdb.cui2pref_name[cui]
-        # Link only this cui to the pref_name
-        cdb.name2cui[name] = {cui}
+        # Link only this cui to the pref_name, if name > 3 chars
+        if len(str(name)) > 3:
+            cdb.name2cui[name] = {cui}
