5f786afa142e4853643b1be291afe3663a827708
w-is-h <w.kraljevic@gmail.com>
2019-09-05 12:21:57 +0000

    Limits

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("./README.md", "r") as fh:
 
 setuptools.setup(
     name="medcat",
-    version="0.2.3.3",
+    version="0.2.3.6",
     author="w-is-h",
     author_email="w.kraljevic@gmail.com",
     description="Concept annotation tool for Electronic Health Records",
