eb35b057720e56a04be37d4b9344c2e7093399a8
w-is-h <w.kraljevic@gmail.com>
2019-09-17 15:27:53 +0000

    DB fixeup

--- webapp/webapp/webapp/settings.py
+++ webapp/webapp/webapp/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'webapp.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'db/db.sqlite3'),
     }
 }
 
