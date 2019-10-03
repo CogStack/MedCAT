9cfdab61b951c08c688627097fe370257e57a093
w-is-h <w.kraljevic@gmail.com>
2019-10-03 15:22:07 +0000

    Fixed the requirements as some don't retain version comp

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("./README.md", "r") as fh:
 
 setuptools.setup(
     name="medcat",
-    version="0.2.5.7",
+    version="0.2.5.8",
     author="w-is-h",
     author_email="w.kraljevic@gmail.com",
     description="Concept annotation tool for Electronic Health Records",
@@ -14,11 +14,11 @@ setuptools.setup(
     url="https://github.com/CogStack/MedCAT",
     packages=['medcat', 'medcat.utils', 'medcat.preprocessing'],
     install_requires=[
-        'numpy',
-        'pandas',
-        'gensim',
-        'spacy',
-        'scipy',],
+        'numpy~=1.15',
+        'pandas~=0.23',
+        'gensim~=3.7',
+        'spacy==2.1.3',
+        'scipy~=1.1',],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
