6734ba18f21bcf86f3181d7e66dbe6b1937af0aa
w-is-h <w.kraljevic@gmail.com>
2019-11-25 18:59:05 +0000

    Set avg as default

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("./README.md", "r") as fh:
 
 setuptools.setup(
     name="medcat",
-    version="0.2.7.4",
+    version="0.2.7.6",
     author="w-is-h",
     author_email="w.kraljevic@gmail.com",
     description="Concept annotation tool for Electronic Health Records",
