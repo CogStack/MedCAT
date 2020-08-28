fe8c67d7665aaeda8d5940e93cee548f9af62e5d
Zeljko <w.kraljevic@gmail.com>
2020-08-28 13:13:12 +0100

    Update make_vocab.py

--- medcat/utils/make_vocab.py
+++ medcat/utils/make_vocab.py
@@ -13,10 +13,28 @@ from medcat.utils.loggers import basic_logger
 log = basic_logger("CAT")
 
 class MakeVocab(object):
+    r'''
+    Create a new vocab from a text file. To make a vocab and train word embeddings do:
+    >>>cdb = <your existing cdb>
+    >>>maker = MakeVocab(cdb=cdb)
+    >>>maker.make(data_iterator, out_folder="./output/")
+    >>>maker.add_vectors(self, in_path="./output/data.txt")
+    >>>
+
+    Args:
+        cdb (medcat.cdb.CDB):
+            The concept database that will be added ontop of the Vocab built from the text file.
+        vocab (medcat.utils.vocab.Vocab, optional):
+            Vocabulary to be extended, leave as None if you want to make a new Vocab. Default: None
+        word_tokenizer (<function>):
+            A custom tokenizer for word spliting - used if embeddings are BERT or similar.
+            Default: None
+    '''
+
     def __init__(self, cdb, vocab=None, word_tokenizer=None):
         self.cdb = cdb
-
         self.w2v = None
+        self.vocab_path = "./vocab.dat"
         if vocab is not None:
             self.vocab = vocab
         else:
@@ -31,15 +49,29 @@ class MakeVocab(object):
         else:
             self.tokenizer = self._tok
 
+            
     def _tok(self, text):
         return [text]
-
-
+    
 
     def make(self, iter_data, out_folder, join_cdb=True):
+        r'''
+        Make a vocab - without vectors initially. This will create two files in the out_folder:
+        - vocab.dat -> The vocabulary without vectors
+        - data.txt -> The tokenized dataset prepared for training of word2vec or similar embeddings. 
+        
+        Args:
+            iter_data (Iterator):
+                An iterator over sentences or documents. Can also be a simple array of text documents/sentences.
+            out_folder (string):
+                A path to a folder where all the results will be saved
+            join_cdb (bool):
+                Should the words from the CDB be added to the Vocab. Default: True
+        '''
         # Save the preprocessed data, used for emb training
         out_path = out_folder + "data.txt"
         vocab_path = out_folder + "vocab.dat"
+        self.vocab_path = vocab_path
         out = open(out_path, 'w')
 
         for ind, doc in enumerate(iter_data):
@@ -76,6 +108,21 @@ class MakeVocab(object):
 
 
     def add_vectors(self, in_path=None, w2v=None, overwrite=False, workers=8, niter=2, min_count=10, window=10, vsize=300):
+        r'''
+        Add vectors to an existing vocabulary and save changes to the vocab_path. 
+        
+        Args:
+            in_path (String):
+                Path to the data.txt that was created by the MakeVocab.make() function.
+            w2v (Word2Vec, optional):
+                An existing word2vec instance. Default: None
+            overwrite (bool):
+                If True it will overwrite existing vectors in the vocabulary. Default: False
+            **: Word2Vec arguments
+        
+        Returns:
+            A trained word2vec model.
+        '''
         if w2v is None:
             data = SimpleIter(in_path)
             w2v = Word2Vec(data, window=window, min_count=min_count, workers=workers, size=vsize, iter=niter)
@@ -87,6 +134,7 @@ class MakeVocab(object):
                 else:
                     if self.vocab.vec(word) is None:
                         self.vocab.add_vec(word, w2v.wv.get_vector(word))
-
-
+        
+        # Save the vocab again, now with vectors
+        self.vocab.save_dict(path=self.vocab_path)        
         return w2v
