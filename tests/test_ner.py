11f6e6c924a919cd6c00a7c55c1693c85aec691e
AlexHandy1 <handy437@gmail.com>
2021-01-12 16:31:27 +0000

    Converted basic NER tests

--- tests/test_ner.py
+++ tests/test_ner.py
@@ -13,120 +13,61 @@ import logging
 from medcat.cdb import CDB
 import os
 import requests
-
-config = Config()
-config.general['log_level'] = logging.INFO
-cdb = CDB(config=config)
-
-nlp = Pipe(tokenizer=spacy_split_all, config=config)
-nlp.add_tagger(tagger=partial(tag_skip_and_punct, config=config),
-               name='skip_and_punct',
-               additional_fields=['is_punct'])
-
-# Add a couple of names
-cdb.add_names(cui='S-229004', names=prepare_name('Movar', nlp, {}, config))
-cdb.add_names(cui='S-229004', names=prepare_name('Movar viruses', nlp, {}, config))
-cdb.add_names(cui='S-229005', names=prepare_name('CDB', nlp, {}, config))
-# Check
-assert cdb.cui2names == {'S-229004': {'movar', 'movarvirus', 'movarviruses'}, 'S-229005': {'cdb'}}
-
-vocab_path = "./tmp_vocab.dat"
-if not os.path.exists(vocab_path):
-    import requests
-    tmp = requests.get("https://s3-eu-west-1.amazonaws.com/zkcl/vocab.dat")
-    with open(vocab_path, 'wb') as f:
-        f.write(tmp.content)
-
-vocab = Vocab.load(vocab_path)
-# Make the pipeline
-nlp = Pipe(tokenizer=spacy_split_all, config=config)
-nlp.add_tagger(tagger=partial(tag_skip_and_punct, config=config),
-               name='skip_and_punct',
-               additional_fields=['is_punct'])
-spell_checker = BasicSpellChecker(cdb_vocab=cdb.vocab, config=config, data_vocab=vocab)
-nlp.add_token_normalizer(spell_checker=spell_checker, config=config)
-ner = NER(cdb, config)
-nlp.add_ner(ner)
-
-# Add Linker
-link = Linker(cdb, vocab, config)
-nlp.add_linker(link)
-
-# Test limits for tokens and uppercase
-config.ner['max_skip_tokens'] = 1
-config.ner['upper_case_limit_len'] = 4
-config.linking['disamb_length_limit'] = 2
-text = "CDB - I was running and then Movar    Virus attacked and CDb"
-d = nlp(text)
-
-assert len(d._.ents) == 2
-assert d._.ents[0]._.link_candidates[0] == 'S-229004'
-
-# Change limit for skip
-config.ner['max_skip_tokens'] = 3
-d = nlp(text)
-assert len(d._.ents) == 3
-
-# Change limit for upper_case
-config.ner['upper_case_limit_len'] = 3
-d = nlp(text)
-assert len(d._.ents) == 4
-
-# Check name length limit
-config.ner['min_name_len'] = 4
-d = nlp(text)
-assert len(d._.ents) == 2
-
-# Speed tests
-from timeit import default_timer as timer
-text = "CDB - I was running and then Movar    Virus attacked and CDb"
-text = text * 300
-config.general['spell_check'] = True
-start = timer()
-for i in range(50):
-    d = nlp(text)
-end = timer()
-print("Time: ", end - start)
-
-# Now without spell check
-config.general['spell_check'] = False
-start = timer()
-for i in range(50):
-    d = nlp(text)
-end = timer()
-print("Time: ", end - start)
+import unittest
 
 
-# Test for linker
-import numpy as np
+class A_NERTests(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        #REVIEW - IS THIS THE SIMPLEST SETUP TO TEST NER
+        print("Set up CDB")
+        cls.config = Config()
+        cls.config.general['log_level'] = logging.INFO
+        cls.cdb = CDB(config=cls.config)
 
-config = Config()
-config.general['log_level'] = logging.DEBUG
-cdb = CDB(config=config)
+        print("Set up Vocab")
+        vocab_path = "./tmp_vocab.dat"
+        if not os.path.exists(vocab_path):
+            tmp = requests.get("https://s3-eu-west-1.amazonaws.com/zkcl/vocab.dat")
+            with open(vocab_path, 'wb') as f:
+                f.write(tmp.content)
 
-# Add a couple of names
-cdb.add_names(cui='S-229004', names=prepare_name('Movar', nlp, {}, config))
-cdb.add_names(cui='S-229004', names=prepare_name('Movar viruses', nlp, {}, config))
-cdb.add_names(cui='S-229005', names=prepare_name('CDB', nlp, {}, config))
-cdb.add_names(cui='S-2290045', names=prepare_name('Movar', nlp, {}, config))
-# Check
-assert cdb.cui2names == {'S-229004': {'movar', 'movarvirus', 'movarviruses'}, 'S-229005': {'cdb'}, 'S-2290045': {'movar'}}
+        cls.vocab = Vocab.load(vocab_path)
 
-cuis = list(cdb.cui2names.keys())
-for cui in cuis[0:50]:
-    vectors = {'short': np.random.rand(300),
-              'long': np.random.rand(300),
-              'medium': np.random.rand(300)
-              }
-    cdb.update_context_vector(cui, vectors, negative=False)
 
-vocab = Vocab.load(vocab_path)
-cm = ContextModel(cdb, vocab, config)
-cm.train_using_negative_sampling('S-229004')
-config.linking['train_count_threshold'] = 0
+        print("Set up NLP pipeline")
+        cls.nlp = Pipe(tokenizer=spacy_split_all, config=cls.config)
+        cls.nlp.add_tagger(tagger=partial(tag_skip_and_punct, config=cls.config),
+                       name='skip_and_punct',
+                       additional_fields=['is_punct'])
+        #CLARIFY WHY CDB.VOCAB HERE 
+        cls.spell_checker = BasicSpellChecker(cdb_vocab=cls.cdb.vocab, config=cls.config, data_vocab=cls.vocab)
+        cls.nlp.add_token_normalizer(spell_checker=cls.spell_checker, config=cls.config)
+        cls.ner = NER(cls.cdb, cls.config)
+        cls.nlp.add_ner(cls.ner)
 
-cm.train('S-229004', d._.ents[1], d)
+        print("Set up Linker")
+        cls.link = Linker(cls.cdb, cls.vocab, cls.config)
+        cls.nlp.add_linker(cls.link)
 
-cm.similarity('S-229004', d._.ents[1], d)
+        print("Set limits for tokens and uppercase")
+        cls.config.ner['max_skip_tokens'] = 1
+        cls.config.ner['upper_case_limit_len'] = 4
+        cls.config.linking['disamb_length_limit'] = 2
 
-cm.disambiguate(['S-2290045', 'S-229004'], d._.ents[1], 'movar', d)
+        print("Add concepts")
+        cls.cdb.add_names(cui='S-229004', names=prepare_name('Movar', cls.nlp, {}, cls.config))
+        cls.cdb.add_names(cui='S-229004', names=prepare_name('Movar viruses', cls.nlp, {}, cls.config))
+        cls.cdb.add_names(cui='S-229005', names=prepare_name('CDB', cls.nlp, {}, cls.config))
+
+    def test_aa_cdb_names_output(self):
+        target_result = {'S-229004': {'movar~virus', 'movar', 'movar~viruses'}, 'S-229005': {'cdb'}}
+        self.assertEqual(self.cdb.cui2names, target_result)
+
+    def test_ab_number_of_entities(self):
+        self.text = "CDB - I was running and then Movar    Virus attacked and CDb"
+        self.text_post_pipe = self.nlp(self.text)
+        self.assertEqual(len(self.text_post_pipe._.ents), 2, "Should equal 2")
+
+if __name__ == '__main__':
+    unittest.main()
