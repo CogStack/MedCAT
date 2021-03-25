d1fd2f2b552b918a63bee0233e8fbe4f13f40abe
w-is-h <w.kraljevic@gmail.com>
2021-03-25 18:23:22 +0000

    none

--- setup_dev.py
+++ setup_dev.py
@@ -9,7 +9,7 @@ with open("./README.md", "r") as fh:
 
 setuptools.setup(
     name="medcat",
-    version="1.0.0dev45",
+    version="1.0.0dev46",
     author="w-is-h",
     author_email="w.kraljevic@gmail.com",
     description="Concept annotation tool for Electronic Health Records",
@@ -30,7 +30,7 @@ setuptools.setup(
         'sklearn~=0.0',
         'elasticsearch~=7.10',
         'dill~=0.3.3',
-        'datasets~=1.3.0',
+        'datasets~=1.5.0',
         ],
     classifiers=[
         "Programming Language :: Python :: 3",
