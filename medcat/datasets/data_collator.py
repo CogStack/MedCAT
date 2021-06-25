caecea4250385b4847cfba0a80abb3b5a895e220
w-is-h <w.kraljevic@gmail.com>
2021-06-25 15:10:17 +0100

    Support for NER+L using BERT

---
+++ medcat/datasets/data_collator.py
@@ -0,0 +1,21 @@
+from typing import Any, Callable, Dict, List, NewType, Optional, Tuple, Union
+import torch
+
+InputDataClass = NewType("InputDataClass", Any)
+
+class CollateAndPadNER(object):
+    def __init__(self, pad_id):
+        self.pad_id = pad_id
+
+    def __call__(self, features: List[InputDataClass]) -> Dict[str, torch.Tensor]:
+        batch = {}
+
+        max_len = max([len(f['input_ids']) for f in features])
+        batch['input_ids'] = torch.tensor([f['input_ids'][0:max_len] + [self.pad_id] *
+            max(0, max_len - len(f['input_ids'])) for f in features], dtype=torch.long)
+        batch['labels'] = torch.tensor([f['labels'][0:max_len] + [-100] *
+            max(0, max_len - len(f['labels'])) for f in features], dtype=torch.long)
+        batch['attention_mask'] = batch['input_ids'] != self.pad_id
+
+
+        return batch
