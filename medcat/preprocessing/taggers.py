3fad158a88874755325ad24387af20a55ccd73ee
baixiac <baixiac@gmail.com>
2021-07-26 17:39:59 +0100

    CAT-18 upgrade spaCy to v3

--- medcat/preprocessing/taggers.py
+++ medcat/preprocessing/taggers.py
@@ -1,30 +1,45 @@
 import re
 
-def tag_skip_and_punct(doc, config):
+def tag_skip_and_punct(nlp, name, config):
     r''' Detects and tags spacy tokens that are punctuation and that should be skipped.
 
-    Args:
-        doc (`spacy.tokens.Doc`):
-            Spacy document that will be tagged.
-        config (`medcat.config.Config`):
-            Global config for medcat.
-
-    Return:
-        (`spacy.tokens.Doc):
-            Tagged spacy document
+     Args:
+         config (`medcat.config.Config`):
+             Global config for medcat.
     '''
-    # Make life easier
-    cnf_p = config.preprocessing
 
-    for token in doc:
-        if config.punct_checker.match(token.lower_) and token.text not in cnf_p['keep_punct']:
-            # There can't be punct in a token if it also has text
-            token._.is_punct = True
-            token._.to_skip = True
-        elif config.word_skipper.match(token.lower_):
-            # Skip if specific strings
-            token._.to_skip = True
-        elif cnf_p['skip_stopwords'] and token.is_stop:
-            token._.to_skip = True
+    return TagSkipAndPunct(nlp, name, config)
 
-    return doc
+class TagSkipAndPunct(object):
+
+    def __init__(self, nlp, name, config):
+        self.nlp = nlp
+        self.name = name
+        self.config = config
+
+    def __call__(self, doc):
+        r''' Detects and tags spacy tokens that are punctuation and that should be skipped.
+
+        Args:
+            doc (`spacy.tokens.Doc`):
+                Spacy document that will be tagged.
+
+        Return:
+            (`spacy.tokens.Doc):
+                Tagged spacy document
+        '''
+        # Make life easier
+        cnf_p = self.config.preprocessing
+
+        for token in doc:
+            if self.config.punct_checker.match(token.lower_) and token.text not in cnf_p['keep_punct']:
+                # There can't be punct in a token if it also has text
+                token._.is_punct = True
+                token._.to_skip = True
+            elif self.config.word_skipper.match(token.lower_):
+                # Skip if specific strings
+                token._.to_skip = True
+            elif cnf_p['skip_stopwords'] and token.is_stop:
+                token._.to_skip = True
+
+        return doc
--- tests/archive_tests/test_ner_archive.py
+++ tests/archive_tests/test_ner_archive.py
@@ -1,3 +1,9 @@
+import logging
+import os
+import unittest
+import numpy as np
+from timeit import default_timer as timer
+from medcat.cdb import CDB
 from medcat.preprocessing.tokenizers import spacy_split_all
 from medcat.ner.vocab_based_ner import NER
 from medcat.preprocessing.taggers import tag_skip_and_punct
@@ -6,127 +12,127 @@ from medcat.utils.normalizers import BasicSpellChecker
 from medcat.vocab import Vocab
 from medcat.preprocessing.cleaners import prepare_name
 from medcat.linking.vector_context_model import ContextModel
-from functools import partial
 from medcat.linking.context_based_linker import Linker
 from medcat.config import Config
-import logging
-from medcat.cdb import CDB
-import os
-import requests
-
-config = Config()
-config.general['log_level'] = logging.INFO
-cdb = CDB(config=config)
-
-nlp = Pipe(tokenizer=spacy_split_all, config=config)
-nlp.add_tagger(tagger=partial(tag_skip_and_punct, config=config),
-               name='skip_and_punct',
-               additional_fields=['is_punct'])
-
-# Add a couple of names
-cdb.add_names(cui='S-229004', names=prepare_name('Movar', nlp, {}, config))
-cdb.add_names(cui='S-229004', names=prepare_name('Movar viruses', nlp, {}, config))
-cdb.add_names(cui='S-229005', names=prepare_name('CDB', nlp, {}, config))
-# Check
-#assert cdb.cui2names == {'S-229004': {'movar', 'movarvirus', 'movarviruses'}, 'S-229005': {'cdb'}}
-
-vocab_path = "./tmp_vocab.dat"
-if not os.path.exists(vocab_path):
-    import requests
-    tmp = requests.get("https://s3-eu-west-1.amazonaws.com/zkcl/vocab.dat")
-    with open(vocab_path, 'wb') as f:
-        f.write(tmp.content)
-
-vocab = Vocab.load(vocab_path)
-# Make the pipeline
-nlp = Pipe(tokenizer=spacy_split_all, config=config)
-nlp.add_tagger(tagger=partial(tag_skip_and_punct, config=config),
-               name='skip_and_punct',
-               additional_fields=['is_punct'])
-spell_checker = BasicSpellChecker(cdb_vocab=cdb.vocab, config=config, data_vocab=vocab)
-nlp.add_token_normalizer(spell_checker=spell_checker, config=config)
-ner = NER(cdb, config)
-nlp.add_ner(ner)
-
-# Add Linker
-link = Linker(cdb, vocab, config)
-nlp.add_linker(link)
-
-# Test limits for tokens and uppercase
-config.ner['max_skip_tokens'] = 1
-config.ner['upper_case_limit_len'] = 4
-config.linking['disamb_length_limit'] = 2
-text = "CDB - I was running and then Movar    Virus attacked and CDb"
-d = nlp(text)
-
-assert len(d._.ents) == 2
-assert d._.ents[0]._.link_candidates[0] == 'S-229004'
-
-# Change limit for skip
-config.ner['max_skip_tokens'] = 3
-d = nlp(text)
-assert len(d._.ents) == 3
-
-# Change limit for upper_case
-config.ner['upper_case_limit_len'] = 3
-d = nlp(text)
-assert len(d._.ents) == 4
-
-# Check name length limit
-config.ner['min_name_len'] = 4
-d = nlp(text)
-assert len(d._.ents) == 2
-
-# Speed tests
-from timeit import default_timer as timer
-text = "CDB - I was running and then Movar    Virus attacked and CDb"
-text = text * 300
-config.general['spell_check'] = True
-start = timer()
-for i in range(50):
-    d = nlp(text)
-end = timer()
-print("Time: ", end - start)
-
-# Now without spell check
-config.general['spell_check'] = False
-start = timer()
-for i in range(50):
-    d = nlp(text)
-end = timer()
-print("Time: ", end - start)
 
 
-# Test for linker
-import numpy as np
+class NerArchiveTests(unittest.TestCase):
 
-config = Config()
-config.general['log_level'] = logging.DEBUG
-cdb = CDB(config=config)
+    def setUp(self) -> None:
+        self.config = Config()
+        self.config.general['log_level'] = logging.INFO
+        cdb = CDB(config=self.config)
 
-# Add a couple of names
-cdb.add_names(cui='S-229004', names=prepare_name('Movar', nlp, {}, config))
-cdb.add_names(cui='S-229004', names=prepare_name('Movar viruses', nlp, {}, config))
-cdb.add_names(cui='S-229005', names=prepare_name('CDB', nlp, {}, config))
-cdb.add_names(cui='S-2290045', names=prepare_name('Movar', nlp, {}, config))
-# Check
-#assert cdb.cui2names == {'S-229004': {'movar', 'movarvirus', 'movarviruses'}, 'S-229005': {'cdb'}, 'S-2290045': {'movar'}}
+        self.nlp = Pipe(tokenizer=spacy_split_all, config=self.config)
+        self.nlp.add_tagger(tagger=tag_skip_and_punct,
+                       name='skip_and_punct',
+                       additional_fields=['is_punct'])
 
-cuis = list(cdb.cui2names.keys())
-for cui in cuis[0:50]:
-    vectors = {'short': np.random.rand(300),
-              'long': np.random.rand(300),
-              'medium': np.random.rand(300)
-              }
-    cdb.update_context_vector(cui, vectors, negative=False)
+        # Add a couple of names
+        cdb.add_names(cui='S-229004', names=prepare_name('Movar', self.nlp, {}, self.config))
+        cdb.add_names(cui='S-229004', names=prepare_name('Movar viruses', self.nlp, {}, self.config))
+        cdb.add_names(cui='S-229005', names=prepare_name('CDB', self.nlp, {}, self.config))
+        # Check
+        #assert cdb.cui2names == {'S-229004': {'movar', 'movarvirus', 'movarviruses'}, 'S-229005': {'cdb'}}
 
-vocab = Vocab.load(vocab_path)
-cm = ContextModel(cdb, vocab, config)
-cm.train_using_negative_sampling('S-229004')
-config.linking['train_count_threshold'] = 0
+        self.vocab_path = "./tmp_vocab.dat"
+        if not os.path.exists(self.vocab_path):
+            import requests
+            tmp = requests.get("https://medcat.rosalind.kcl.ac.uk/media/vocab.dat")
+            with open(self.vocab_path, 'wb') as f:
+                f.write(tmp.content)
 
-cm.train('S-229004', d._.ents[1], d)
+        vocab = Vocab.load(self.vocab_path)
+        # Make the pipeline
+        self.nlp = Pipe(tokenizer=spacy_split_all, config=self.config)
+        self.nlp.add_tagger(tagger=tag_skip_and_punct,
+                       name='skip_and_punct',
+                       additional_fields=['is_punct'])
+        spell_checker = BasicSpellChecker(cdb_vocab=cdb.vocab, config=self.config, data_vocab=vocab)
+        self.nlp.add_token_normalizer(spell_checker=spell_checker, config=self.config)
+        ner = NER(cdb, self.config)
+        self.nlp.add_ner(ner)
 
-cm.similarity('S-229004', d._.ents[1], d)
+        # Add Linker
+        link = Linker(cdb, vocab, self.config)
+        self.nlp.add_linker(link)
 
-cm.disambiguate(['S-2290045', 'S-229004'], d._.ents[1], 'movar', d)
+        self.text = "CDB - I was running and then Movar    Virus attacked and CDb"
+
+    def test_limits_for_tokens_and_uppercase(self):
+        self.config.ner['max_skip_tokens'] = 1
+        self.config.ner['upper_case_limit_len'] = 4
+        self.config.linking['disamb_length_limit'] = 2
+
+        d = self.nlp(self.text)
+
+        assert len(d._.ents) == 2
+        assert d._.ents[0]._.link_candidates[0] == 'S-229004'
+
+    def test_change_limit_for_skip(self):
+        self.config.ner['max_skip_tokens'] = 3
+        d = self.nlp(self.text)
+        assert len(d._.ents) == 3
+
+    def test_change_limit_for_upper_case(self):
+        self.config.ner['upper_case_limit_len'] = 3
+        d = self.nlp(self.text)
+        assert len(d._.ents) == 4
+
+    def test_check_name_length_limit(self):
+        self.config.ner['min_name_len'] = 4
+        d = self.nlp(self.text)
+        assert len(d._.ents) == 2
+
+    def test_speed(self):
+        text = "CDB - I was running and then Movar    Virus attacked and CDb"
+        text = text * 300
+        self.config.general['spell_check'] = True
+        start = timer()
+        for i in range(50):
+            d = self.nlp(text)
+        end = timer()
+        print("Time: ", end - start)
+
+    def test_without_spell_check(self):
+        # Now without spell check
+        self.config.general['spell_check'] = False
+        start = timer()
+        for i in range(50):
+            d = self.nlp(self.text)
+        end = timer()
+        print("Time: ", end - start)
+
+
+    def test_for_linker(self):
+        self.config = Config()
+        self.config.general['log_level'] = logging.DEBUG
+        cdb = CDB(config=self.config)
+
+        # Add a couple of names
+        cdb.add_names(cui='S-229004', names=prepare_name('Movar', self.nlp, {}, self.config))
+        cdb.add_names(cui='S-229004', names=prepare_name('Movar viruses', self.nlp, {}, self.config))
+        cdb.add_names(cui='S-229005', names=prepare_name('CDB', self.nlp, {}, self.config))
+        cdb.add_names(cui='S-2290045', names=prepare_name('Movar', self.nlp, {}, self.config))
+        # Check
+        #assert cdb.cui2names == {'S-229004': {'movar', 'movarvirus', 'movarviruses'}, 'S-229005': {'cdb'}, 'S-2290045': {'movar'}}
+
+        cuis = list(cdb.cui2names.keys())
+        for cui in cuis[0:50]:
+            vectors = {'short': np.random.rand(300),
+                      'long': np.random.rand(300),
+                      'medium': np.random.rand(300)
+                      }
+            cdb.update_context_vector(cui, vectors, negative=False)
+
+        d = self.nlp(self.text)
+        vocab = Vocab.load(self.vocab_path)
+        cm = ContextModel(cdb, vocab, self.config)
+        cm.train_using_negative_sampling('S-229004')
+        self.config.linking['train_count_threshold'] = 0
+
+        cm.train('S-229004', d._.ents[1], d)
+
+        cm.similarity('S-229004', d._.ents[1], d)
+
+        cm.disambiguate(['S-2290045', 'S-229004'], d._.ents[1], 'movar', d)
