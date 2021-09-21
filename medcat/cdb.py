763382f4be32e9f9d3a436bd807a4c313c14262e
Xi Bai <baixiac@gmail.com>
2021-09-21 16:56:44 +0100

    NO-TICKET support old CDBs missing max document length

--- medcat/cdb.py
+++ medcat/cdb.py
@@ -674,6 +674,8 @@ class CDB(object):
         weighted_average_function = config.linking['weighted_average_function']
         if callable(weighted_average_function) and getattr(weighted_average_function, "__name__", None) == "<lambda>":
             config.linking['weighted_average_function'] = partial(weighted_average, factor=0.0004)
+        if getattr(config.preprocessing, 'max_document_length', None) is None:
+            config.preprocessing['max_document_length'] = 1000000
         if getattr(config.ner, 'workers', None) is None:
             config.ner['workers'] = workers()
         if getattr(config.linking, 'workers', None) is None:
