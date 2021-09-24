a5fc74a7b1976f1cc418dcca136eead62d1ad726
Xi Bai <baixiac@gmail.com>
2021-09-24 12:32:23 +0100

    CAT-58 improve memory usage

--- tests/test_config.py
+++ tests/test_config.py
@@ -10,6 +10,10 @@ class ConfigTests(unittest.TestCase):
         with tempfile.TemporaryFile() as f:
             pickle.dump(Config(), f)
 
+    def test_from_dict(self):
+        config = Config.from_dict({"key": "value"})
+        self.assertEqual("value", config.key)
+
 
 if __name__ == '__main__':
     unittest.main()
