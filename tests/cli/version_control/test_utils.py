e09a42019ac8e4772586a088d26a4dfd3d1ac4a7
vladd-bit <vlad.a.dinu@gmail.com>
2021-11-09 11:35:38 +0000

    Util test fixes.

--- tests/cli/version_control/test_utils.py
+++ tests/cli/version_control/test_utils.py
@@ -4,6 +4,9 @@ from medcat.cli.system_utils import *
 from medcat.cli.config import *
 
 
+ordered, compare = make_orderer()
+unittest.defaultTestLoader.sortTestMethodsUsing = compare
+
 class UtilTests(unittest.TestCase):
     
     """
@@ -22,21 +25,26 @@ class UtilTests(unittest.TestCase):
     def setUp(self):
         pass
     
+    @ordered
     def test_access_config(self):
         self.assertNotEqual(get_auth_environment_vars(), False)
 
+    @ordered
     def test_models_folder_access(self):
         result = get_local_model_storage_path()
         self.assertNotEqual(result, "")
    
+    @ordered
     def test_create_model_tag_folder(self):
         if(os.path.exists(UtilTests.tmp_full_model_tag_name)):
             force_delete_path(UtilTests.new_model_package_folder)
-        self.assertEqual(create_model_folder(self.tmp_full_model_tag_name), True)
+        self.assertEqual(create_model_folder(UtilTests.tmp_full_model_tag_name), True)
 
+    @ordered
     def test_create_new_base_repository(self):
         self.assertEqual(create_new_base_repository(repo_folder_path=UtilTests.new_model_package_folder, git_repo_url=get_auth_environment_vars()["git_repo_url"]), True)
 
+    @ordered
     def test_model_tag_folder_is_git_repository(self):
         self.assertEqual(is_dir_git_repository(UtilTests.new_model_package_folder), True)
 
