700c0e7332a029127956d7291852c7e155dfac38
Xi Bai <82581439+baixiac@users.noreply.github.com>
2022-01-20 14:15:27 +0000

    Add the online form for model download (#186)
    * CAT-102 add the online form required by model download
    
    * CAT-102 update the form info
    
    * CAT-102 cosmetic change

--- webapp/webapp/demo/models.py
+++ webapp/webapp/demo/models.py
@@ -1,6 +1,16 @@
 from django.db import models
+from django.core.exceptions import ValidationError
 
 # Create your models here.
 class UploadedText(models.Model):
     text = models.TextField(default="", blank=True)
     create_time = models.DateTimeField(auto_now_add=True)
+
+
+class Downloader(models.Model):
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    email = models.EmailField(max_length=50)
+    affiliation = models.CharField(max_length=100)
+    funder = models.CharField(max_length=100, blank=True, default="")
+    use_case = models.TextField(max_length=200)
