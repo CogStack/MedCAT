2d7ceb3d9c9d57a65e4e445b74315ed587b4abb7
Xi Bai <baixiac@gmail.com>
2022-02-10 16:07:03 +0000

    CAT-93 set default steps to 100K for checkpointing

--- medcat/config.py
+++ medcat/config.py
@@ -232,8 +232,8 @@ class Config(ConfigMixin):
                 'checkpoint': {
                     # When doing training this is the name of the directory where checkpoints will be saved
                     'output_dir': 'checkpoints',
-                    # When training how often to save the checkpoint (one step == one document)
-                    'steps': 1000,
+                    # When training how often to save the checkpoint (one step represents one document)
+                    'steps': 100000,
                     # When training the maximum checkpoints will be kept on the disk
                     "max_to_keep": 1,
                 },
