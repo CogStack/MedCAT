e59a80c5a382f2f124f6ad1c15894536d3663b18
Xi Bai <baixiac@gmail.com>
2022-03-02 16:13:06 +0000

    CU-1zqb1ej expose /media and make db backup more configurable

--- webapp/webapp/webapp/urls.py
+++ webapp/webapp/webapp/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('demo.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
