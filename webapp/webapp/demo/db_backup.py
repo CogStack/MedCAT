2711030d3420371b666bc68ccdb4de94d42a4d88
Xi Bai <baixiac@gmail.com>
2022-03-02 16:13:06 +0000

    CU-1zqb1ej expose /media and make db backup more configurable

--- webapp/webapp/demo/db_backup.py
+++ webapp/webapp/demo/db_backup.py
@@ -6,8 +6,8 @@ from django_cron import CronJobBase, Schedule
 
 class DbBackup(CronJobBase):
 
-    RUN_EVERY_MINS = 60 * 24
-    RETRY_AFTER_FAILURE_MINS = 5
+    RUN_EVERY_MINS = int(os.environ.get("DB_BACKUP_EVERY_MINS", 60 * 12))
+    RETRY_AFTER_FAILURE_MINS = int(os.environ.get("DB_BACKUP_RETRY_BACKOFF_MINS", 5))
 
     schedule = Schedule(run_every_mins=RUN_EVERY_MINS, retry_after_failure_mins=RETRY_AFTER_FAILURE_MINS)
     code = "demo.db_backup.DbBackup"
--- webapp/webapp/webapp/settings.py
+++ webapp/webapp/webapp/settings.py
@@ -141,3 +141,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'data')
