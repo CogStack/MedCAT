48ab4c0adfb4b3190f4ab9f640807261954e7c03
Xi Bai <baixiac@gmail.com>
2022-03-04 12:18:45 +0000

    CU-1zqb1ej set static root and collect statics during docker build

--- webapp/webapp/webapp/settings.py
+++ webapp/webapp/webapp/settings.py
@@ -141,6 +141,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'demo', 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'data')
