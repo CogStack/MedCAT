d2175e6689374860e88a48d0d4939c80fceb512b
w-is-h <w.kraljevic@gmail.com>
2022-06-16 14:44:58 +0100

    Option to remove everything from the db

--- webapp/webapp/demo/admin.py
+++ webapp/webapp/demo/admin.py
@@ -1,7 +1,16 @@
 from django.contrib import admin
 from .models import *
 
-# Register your models here.
-admin.site.register(UploadedText)
 admin.site.register(Downloader)
 admin.site.register(MedcatModel)
+
+def remove_text(modeladmin, request, queryset):
+    UploadedText.objects.all().delete()
+
+class UploadedTextAdmin(admin.ModelAdmin):
+    model = UploadedText
+    actions = [remove_text]
+
+# Register your models here.
+admin.site.register(UploadedText, UploadedTextAdmin)
+
