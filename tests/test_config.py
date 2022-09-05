f0cdf78f00c273181d8eff0f211daea88b5ec717
Mart drives_a_ford Ratas <mart.ratas@gmail.com>
2022-09-05 11:48:10 +0100

    Added extra tests to make sure all the config keys are available through both the legacy getitem method as well as through the attribute based method

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,7 +1,7 @@
 import unittest
 import pickle
 import tempfile
-from medcat.config import Config
+from medcat.config import Config, MixingConfig
 
 
 class ConfigTests(unittest.TestCase):
@@ -10,9 +10,42 @@ class ConfigTests(unittest.TestCase):
         c = Config()
         self.assertIsNotNone(c.general.spacy_model)
 
+    def check_MixingConfig(self, c: MixingConfig):
+        pass
+
+    def test_identifier_works_all(self, non_default_non_none=['word_skipper', 'punct_checker']):
+        # Ignoring work_skipper and punct checker since their values
+        # are not default, but they are also not None
+        def check_recursively(c: MixingConfig):
+            for field_name, field in c.fields().items():
+                with self.subTest(f'{type(c).__name__}:{field_name}'):
+                    val = getattr(c, field_name)
+                    if field_name in non_default_non_none:
+                        self.assertIsNotNone(val)
+                    else:
+                        self.assertEqual(val, field.default)
+                if isinstance(val, MixingConfig):
+                    check_recursively(val)
+        check_recursively(Config())
+
     def test_legacy_getitem_works(self):
         c = Config()
         self.assertIsNotNone(c.general['spacy_model'])
+
+    def test_getitem_works_all(self, non_default_non_none=['word_skipper', 'punct_checker']):
+        # Ignoring work_skipper and punct checker since their values
+        # are not default, but they are also not None
+        def check_recursively(c: MixingConfig):
+            for field_name, field in c.fields().items():
+                with self.subTest():
+                    val = c[field_name]
+                    if field_name in non_default_non_none:
+                        self.assertIsNotNone(val)
+                    else:
+                        self.assertEqual(val, field.default)
+                if isinstance(val, MixingConfig):
+                    check_recursively(val)
+        check_recursively(Config())
 
     def test_creates_new_defaults(self):
         # the default values are mutable
@@ -25,6 +58,12 @@ class ConfigTests(unittest.TestCase):
         bc2 = Config()
         print(bc2.general.spacy_model, 'vs', bc1.general.spacy_model)
         self.assertNotEqual(bc2.general.spacy_model, bc1.general.spacy_model)
+
+    def test_lists_are_different(self):
+        bc1 = Config()
+        bc1.version.history.append('v3')
+        bc2 = Config()
+        self.assertNotEqual(bc1.version.history, bc2.version.history)
 
     def test_uses_different_internal_instances(self):
         # again, if the instance for .general was the same,
