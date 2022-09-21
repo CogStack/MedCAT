facfdd22021a89f7e380e069bc1112d8a066662e
mart-r <mart.ratas@gmail.com>
2022-09-21 12:39:53 +0100

    CU-2vzhd93 Add package level method that adds default log handlers to a logger

--- medcat/__init__.py
+++ medcat/__init__.py
@@ -6,7 +6,33 @@ import logging
 # Make sure the default behaviour is to _not_ log anything
 # The idea is to let the user of the library decide where,
 # what and how they want to log information from the library
-logging.getLogger(__name__).addHandler(logging.NullHandler())
+logger = logging.getLogger(__name__)
+logger.addHandler(logging.NullHandler())
+
+def add_default_log_handlers(log: logging.Logger = logger, target_file : str = 'medcat.log') -> None:
+    """Add default log handlers to the specified logger.
+    This method will add a file handler that will write
+    into `target_file` (defaults to 'medcat.log') and
+    a console handler that outputs to the console.
+
+    Args:
+        log (logging.Logger): The logger to add the handlers to. Defaults to logger.
+        target_file (str): The target file for file handler. Defaults to 'medcat.log'.
+
+    Returns:
+        _type_: _description_
+    """
+    if len(log.handlers) == 0: # If we do not have any handlers add them
+        # create a file handler
+        fh = logging.FileHandler(target_file)
+        # create console handler
+        ch = logging.StreamHandler()
+
+        # add the handlers to the logger
+        log.addHandler(fh)
+        log.addHandler(ch)
+
+    return log
 
 name = 'medcat'
 
