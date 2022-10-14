7d29b70b6dbf66d3a375372fd66b645b8da1410b
mart-r <mart.ratas@gmail.com>
2022-10-14 10:13:37 +0100

    CU-2wgnqg5 Add (optional) per-phrase results to results/reporting

--- medcat/utils/regression/results.py
+++ medcat/utils/regression/results.py
@@ -1,9 +1,9 @@
 
-from typing import List
+from typing import Dict, List
 import pydantic
 
 
-class ResultDescriptor(pydantic.BaseModel):
+class SingleResultDescriptor(pydantic.BaseModel):
     name: str
     """The name of the part that was checked"""
     success: int = 0
@@ -11,20 +11,16 @@ class ResultDescriptor(pydantic.BaseModel):
     fail: int = 0
     """Number of failures"""
 
-    def report(self, cui: str, name: str, phrase: str, success: bool) -> None:
+    def report(self, success: bool) -> None:
         """Report a test case and its successfulness
 
         Args:
-            cui (str): The CUI being checked
-            name (str): The name being checked
-            phrase (str): The phrase being checked
             success (bool): Whether or not the check was successful
         """
         if success:
             self.success += 1
         else:
             self.fail += 1
-        # TODO (optionally) keep track of other information
 
     def get_report(self) -> str:
         """Get the report associated with this descriptor
@@ -36,6 +32,41 @@ class ResultDescriptor(pydantic.BaseModel):
         return f"""Tested "{self.name}" for a total of {total} cases:
         Success:    {self.success:10d} ({100 * self.success / total if total > 0 else 0}%)
         Failure:    {self.fail:10d} ({100 * self.fail / total if total > 0 else 0}%)"""
+
+
+class ResultDescriptor(SingleResultDescriptor):
+    per_phrase_results: Dict[str, SingleResultDescriptor] = {}
+
+    def report(self, cui: str, name: str, phrase: str, success: bool) -> None:
+        """Report a test case and its successfulness
+
+        Args:
+            cui (str): The CUI being checked
+            name (str): The name being checked
+            phrase (str): The phrase being checked
+            success (bool): Whether or not the check was successful
+        """
+        super().report(success)
+        if phrase not in self.per_phrase_results:
+            self.per_phrase_results[phrase] = SingleResultDescriptor(
+                name=phrase)
+        self.per_phrase_results[phrase].report(success)
+
+    def get_report(self, phrases_separately: bool = False) -> str:
+        """Get the report associated with this descriptor
+
+        Args:
+            phrases_separately (bool): Whether to output descriptor for each phrase separately
+
+        Returns:
+            str: The report string
+        """
+        sr = super().get_report()
+        if not phrases_separately:
+            return sr
+        children = '\n'.join([srd.get_report()
+                             for srd in self.per_phrase_results.values()])
+        return sr + '\n\t\t' + children.replace('\n', '\n\t\t')
 
 
 class MultiDescriptor(pydantic.BaseModel):
