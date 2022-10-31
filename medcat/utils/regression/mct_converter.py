f577c17e6e84fbfd7363db3c81daa63a11a737f2
mart-r <mart.ratas@gmail.com>
2022-10-31 18:08:14 +0000

    CU-2wgnqg5 Add use of context selector to runnable

--- medcat/utils/regression/mct_converter.py
+++ medcat/utils/regression/mct_converter.py
@@ -3,20 +3,26 @@ import argparse
 import logging
 import os
 from pathlib import Path
+from typing import List, Optional
 
-from medcat.utils.regression.converting import medcat_export_json_to_regression_yml
+from medcat.utils.regression.converting import PerSentenceSelector, PerWordContextSelector, medcat_export_json_to_regression_yml
 
 
 logger = logging.getLogger(__name__)
 
 
-def main(mct_export: str, target: str, overwrite: bool = False) -> None:
+def main(mct_export: str, target: str, overwrite: bool = False,
+         words: Optional[List[int]] = None) -> None:
     if not overwrite and os.path.isfile(target):
         raise ValueError("Not able to overwrite an existingfile, "
                          "pass '--overwrite' to force an overwrite")
     logger.info(
         "Starting to convert export JSON to YAML from file %s", mct_export)
-    yaml = medcat_export_json_to_regression_yml(mct_export)
+    if not words:
+        cont_sel = PerSentenceSelector()
+    else:
+        cont_sel = PerWordContextSelector(*words)
+    yaml = medcat_export_json_to_regression_yml(mct_export, cont_sel=cont_sel)
     logger.debug("Conversion successful")
     logger.info("Saving writing data to %s", target)
     with open(target, 'w') as f:
@@ -35,6 +41,9 @@ if __name__ == '__main__':
                         action='store_true')
     parser.add_argument(
         '--overwrite', help='Overwrite the target file if it exists', action='store_true')
+    parser.add_argument(
+        '--words', help='Select the number of words to select from before and after the concept',
+        nargs=2, type=int)
     args = parser.parse_args()
     if not args.silent:
         logger.addHandler(logging.StreamHandler())
@@ -43,4 +52,4 @@ if __name__ == '__main__':
         from checking import logger as checking_logger
         checking_logger.addHandler(logging.StreamHandler())
         checking_logger.setLevel('DEBUG')
-    main(args.file, args.target, overwrite=args.overwrite)
+    main(args.file, args.target, overwrite=args.overwrite, words=args.words)
