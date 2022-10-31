bcf3cb7173d82e778f173c2ac6fa951d71b428f5
mart-r <mart.ratas@gmail.com>
2022-10-31 16:47:10 +0000

    CU-2wgnqg5 Add for number of cases read from MCT export

--- tests/utils/regression/test_mct_2_yml.py
+++ tests/utils/regression/test_mct_2_yml.py
@@ -1,8 +1,23 @@
+import json
 import unittest
 import yaml
+
 from medcat.utils.regression.checking import RegressionChecker
 
 from medcat.utils.regression.converting import medcat_export_json_to_regression_yml
+from medcat.utils.regression.targeting import TargetInfo
+
+
+class FakeTranslationLayer:
+
+    def __init__(self, mct_export: dict) -> None:
+        self.mct_export = json.loads(mct_export)
+
+    def all_targets(self):  # -> Iterator[TargetInfo]:
+        for project in self.mct_export['projects']:
+            for doc in project['documents']:
+                for ann in doc['annotations']:
+                    yield TargetInfo(ann['cui'], ann['value'])
 
 
 class TestConversion(unittest.TestCase):
@@ -27,3 +42,14 @@ class TestConversion(unittest.TestCase):
         d = yaml.safe_load(converted_yaml)
         checker = RegressionChecker.from_dict(d)
         self.assertIsInstance(checker, RegressionChecker)
+
+    def test_correct_number_of_cases(self):
+        converted_yaml = medcat_export_json_to_regression_yml(
+            self.def_file_name)
+        checker = RegressionChecker.from_dict(yaml.safe_load(converted_yaml))
+        with open(self.def_file_name, 'r') as f:
+            mct_export = f.read()
+            expected = mct_export.count('"cui":')
+        nr_of_cases = len(list(checker.get_all_subcases(
+            FakeTranslationLayer(mct_export))))
+        self.assertEqual(nr_of_cases, expected)
