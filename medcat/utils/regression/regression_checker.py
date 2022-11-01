fcb03f7743d6502ac02a27ffa1fd385001a114d0
mart-r <mart.ratas@gmail.com>
2022-11-01 12:06:01 +0000

    CU-2wgnqg5 Fix verbose output mode/logging

--- medcat/utils/regression/regression_checker.py
+++ medcat/utils/regression/regression_checker.py
@@ -76,9 +76,13 @@ if __name__ == '__main__':
         logger.addHandler(logging.StreamHandler())
         logger.setLevel('INFO')
     if args.verbose:
-        from checking import logger as checking_logger
-        checking_logger.addHandler(logging.StreamHandler())
+        from medcat.utils.regression.checking import logger as checking_logger
+        from medcat.utils.regression.converting import logger as converting_logger
+        console = logging.StreamHandler()
+        checking_logger.addHandler(console)
         checking_logger.setLevel('DEBUG')
+        converting_logger.addHandler(console)
+        converting_logger.setLevel('DEBUG')
     main(args.modelpack, args.test_suite,
          max_failures=args.maxfail, total=args.total, report=args.report,
          phrases=args.phrases, hide_empty=args.noempty)
