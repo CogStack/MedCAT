0f8db472191524e4e8f018f12193e5b5519c481b
mart-r <mart.ratas@gmail.com>
2022-11-14 14:43:54 +0000

    CU-2wgnqg5 Add json output to regression checking

--- medcat/utils/regression/regression_checker.py
+++ medcat/utils/regression/regression_checker.py
@@ -1,4 +1,5 @@
 import argparse
+import json
 from pathlib import Path
 import logging
 
@@ -13,7 +14,9 @@ logger = logging.getLogger(__name__)
 def main(model_pack_dir: Path, test_suite_file: Path,
          total: Optional[int] = None,
          phrases: bool = False, hide_empty: bool = False,
-         hide_failures: bool = False) -> None:
+         hide_failures: bool = False,
+         jsonpath: Optional[Path] = None, overwrite: bool = False,
+         jsonindent: Optional[int] = None) -> None:
     """Check test suite against the specifeid model pack.
 
     Args:
@@ -23,15 +26,29 @@ def main(model_pack_dir: Path, test_suite_file: Path,
         phrases (bool): Whether to show per-phrase information in a report
         hide_empty (bool): Whether to hide empty cases in a report
         hide_failures (bool): Whether to hide failures in a report
+        jsonpath (Optional[Path]): The json path to save the report to (if specified)
+        overwrite (bool): Whether to overwrite the file if it exists. Defaults to False
+        jsonindent (int): The indentation for json objects. Defaults to 0
     """
+    if jsonpath and jsonpath.exists() and not overwrite:
+        # check before doing anything so as to not waste time on the tests
+        raise ValueError(
+            f'Unable to write to existing file {str(jsonpath)} pass --overwrite to overwrite the file')
+    if jsonpath and not jsonpath.parent.exists():
+        raise ValueError(
+            f'Need to specify a file in an existing directory, folder not found: {str(jsonpath)}')
     logger.info('Loading RegressionChecker from yaml: %s', test_suite_file)
     rc = RegressionChecker.from_yaml(str(test_suite_file))
     logger.info('Loading model pack from file: %s', model_pack_dir)
     cat: CAT = CAT.load_model_pack(str(model_pack_dir))
     logger.info('Checking the current status')
     res = rc.check_model(cat, TranslationLayer.from_CDB(cat.cdb), total=total)
-    logger.info(res.get_report(phrases_separately=phrases,
-                hide_empty=hide_empty, show_failures=not hide_failures))
+    if jsonpath:
+        logger.info('Writing to %s', str(jsonpath))
+        jsonpath.write_text(json.dumps(res.dict(), indent=jsonindent))
+    else:
+        logger.info(res.get_report(phrases_separately=phrases,
+                    hide_empty=hide_empty, show_failures=not hide_failures))
 
 
 if __name__ == '__main__':
@@ -57,6 +74,12 @@ if __name__ == '__main__':
                         action='store_true')
     parser.add_argument('--hidefailures', help='Hide failed cases in report',
                         action='store_true')
+    parser.add_argument('--jsonfile', help='Save report to a json file',
+                        type=Path)
+    parser.add_argument('--overwrite', help='Whether to overwrite save file',
+                        action='store_true')
+    parser.add_argument('--jsonindent', help='The json indent',
+                        type=int, default=None)
     args = parser.parse_args()
     if not args.silent:
         logger.addHandler(logging.StreamHandler())
@@ -66,4 +89,5 @@ if __name__ == '__main__':
         regr_logger.setLevel('DEBUG')
         regr_logger.addHandler(logging.StreamHandler())
     main(args.modelpack, args.test_suite, total=args.total,
-         phrases=args.phrases, hide_empty=args.noempty, hide_failures=args.hidefailures)
+         phrases=args.phrases, hide_empty=args.noempty, hide_failures=args.hidefailures,
+         jsonpath=args.jsonfile, overwrite=args.overwrite, jsonindent=args.jsonindent)
