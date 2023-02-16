a2c38cebe0397e10fb45ea17d543f57e55cffff6
mart-r <mart.ratas@gmail.com>
2023-02-16 17:20:07 +0000

    Add more comprehensive versioning tests

--- tests/utils/test_versioning.py
+++ tests/utils/test_versioning.py
@@ -9,6 +9,19 @@ from medcat.vocab import Vocab
 from .regression.test_metadata import MODEL_CARD_EXAMPLE, EXAMPLE_VERSION
 
 
+CORRECT_SEMANTIC_VERSIONS = [("1.0.1-alpha-1", (1, 0, 1)), ("0.0.1-alpha-1", (0, 0, 1)),
+                             ("1.0.0-alpha.1", (1, 0, 0)
+                              ), ("1.0.0-0.3.7", (1, 0, 0)),
+                             ("1.0.0-x.7.z.92", (1, 0, 0)
+                              ), ("1.0.0-x-y-z.--", (1, 0, 0)),
+                             ("1.0.0-alpha+001", (1, 0, 0)
+                              ), ("1.0.0+20130313144700", (1, 0, 0)),
+                             ("1.0.0-beta+exp.sha.5114f85", (1, 0, 0)),
+                             ("1.0.0+21AF26D3----117B344092BD", (1, 0, 0))]
+INCORRECT_SEMANTIC_VERSIONS = ["01.0.0", "0.01.0", "0.0.01", "0.0.0\nSOMETHING",
+                               "1.0.space", "1.0.0- space"]
+
+
 class VersionGettingFromModelCardTests(unittest.TestCase):
     FAKE_MODEL_CARD1 = {"Something": "value"}
     FAKE_MODEL_CARD2 = {"MedCAT Version": "not semantic"}
@@ -26,9 +39,32 @@ class VersionGettingFromModelCardTests(unittest.TestCase):
             get_version_from_modelcard(self.FAKE_MODEL_CARD1)
 
     def test_fails_upon_model_card_with_incorrect_version(self):
+        cntr = 0
         for fake_model_card in self.WRONG_VERSION_FAKE_MODELS:
             with self.assertRaises(ValueError):
                 get_version_from_modelcard(fake_model_card)
+            cntr += 1
+        self.assertEqual(cntr, len(self.WRONG_VERSION_FAKE_MODELS))
+
+    def test_fails_upon_wrong_version(self):
+        cntr = 0
+        for wrong_version in INCORRECT_SEMANTIC_VERSIONS:
+            d = {"MedCAT Version": wrong_version}
+            with self.subTest(f"With version: {wrong_version}"):
+                with self.assertRaises(ValueError):
+                    get_version_from_modelcard(d)
+                cntr += 1
+        self.assertEqual(cntr, len(INCORRECT_SEMANTIC_VERSIONS))
+
+    def test_gets_version_from_correct_versions(self):
+        cntr = 0
+        for version, expected in CORRECT_SEMANTIC_VERSIONS:
+            d = {"MedCAT Version": version}
+            with self.subTest(f"With version: {version}"):
+                got_version = get_version_from_modelcard(d)
+                self.assertEqual(got_version, expected)
+                cntr += 1
+        self.assertEqual(cntr, len(CORRECT_SEMANTIC_VERSIONS))
 
 
 class VersionGettingFromCATTests(unittest.TestCase):
@@ -39,7 +75,8 @@ class VersionGettingFromCATTests(unittest.TestCase):
         self.vocab = Vocab.load(os.path.join(os.path.dirname(
             os.path.realpath(__file__)), "..", "..", "examples", "vocab.dat"))
         self.cdb.config.general.spacy_model = "en_core_web_md"
-        self.cdb.config.version.medcat_version = ".".join(str(v) for v in EXAMPLE_VERSION)
+        self.cdb.config.version.medcat_version = ".".join(
+            str(v) for v in EXAMPLE_VERSION)
         self.cdb.config.ner.min_name_len = 2
         self.cdb.config.ner.upper_case_limit_len = 3
         self.cdb.config.general.spell_check = True
