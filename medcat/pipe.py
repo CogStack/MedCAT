850e24dc5318fa72aca523c2ae00d69f23ef0293
Sathianphong Phongsathian <jo.s2p2@gmail.com>
2023-06-06 16:24:18 +0700

    Remove unnecessary import and comments
    Remove unnecessary import and comments

--- medcat/pipe.py
+++ medcat/pipe.py
@@ -17,9 +17,6 @@ from medcat.config import Config
 from medcat.pipeline.pipe_runner import PipeRunner
 from medcat.preprocessing.taggers import tag_skip_and_punct
 from medcat.ner.transformers_ner import TransformersNER
-import spacy
-from spacy.lang.th import Thai
-import spacy_pythainlp.core
 
 
 logger = logging.getLogger(__name__) # different logger from the package-level one
@@ -41,28 +38,6 @@ class Pipe(object):
     """
 
     def __init__(self, tokenizer: Tokenizer, config: Config) -> None:
-        #self._nlp = spacy.blank('th')
-        #self._nlp.add_pipe('pythainlp')
-        # self._nlp = Thai()
-        # self._nlp.add_pipe(
-        #     "pythainlp", 
-        #     config={
-        #         "pos_engine": "perceptron",
-        #         "pos": False,
-        #         "pos_corpus": "orchid_ud",
-        #         "sent_engine": "crfcut",
-        #         "sent": False,
-        #         "ner_engine": "thainer",
-        #         "ner": False,
-        #         "tokenize_engine": "newmm",
-        #         "tokenize": False,
-        #         "dependency_parsing": False,
-        #         "dependency_parsing_engine": "esupar",
-        #         "dependency_parsing_model": None,
-        #         "word_vector": False,
-        #         "word_vector_model": "thai2fit_wv"
-        #     }
-        # )
         self._nlp = spacy.load(config.general.spacy_model, disable=config.general.spacy_disabled_components)
         if config.preprocessing.stopwords is not None:
             self._nlp.Defaults.stop_words = set(config.preprocessing.stopwords)
