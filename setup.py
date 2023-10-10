a7e698d5e3e4716e303d270a2ab7f09b60286b0c
mart-r <mart.ratas@gmail.com>
2023-10-10 16:09:44 +0100

    CU-8692wbcq5: Undo pinning numpy to max version in setup.py

--- setup.py
+++ setup.py
@@ -18,9 +18,7 @@ setuptools.setup(
               'medcat.tokenizers', 'medcat.utils.meta_cat', 'medcat.pipeline', 'medcat.utils.ner',
               'medcat.utils.saving', 'medcat.utils.regression'],
     install_requires=[
-        # the max version is for scipy support while building docs
-        # (doesn't seem to affect other stuff)
-        'numpy>=1.22.0,<1.26.0', # first to support 3.11
+        'numpy>=1.22.0', # first to support 3.11
         'pandas>=1.4.2', # first to support 3.11
         'gensim>=4.3.0', # first to support 3.11
         'spacy>=3.1.0',
