e25bc97527a0e341a5401da53432f6c74d9e08ad
mart-r <mart.ratas@gmail.com>
2024-01-23 09:49:34 +0000

    CU-8693kp0gw: Pin more recent versions for major dependencies; Avoid major bumps where applicable

--- setup.py
+++ setup.py
@@ -18,18 +18,18 @@ setuptools.setup(
               'medcat.tokenizers', 'medcat.utils.meta_cat', 'medcat.pipeline', 'medcat.utils.ner',
               'medcat.utils.saving', 'medcat.utils.regression', 'medcat.stats'],
     install_requires=[
-        'numpy>=1.22.0', # first to support 3.11
+        'numpy>=1.22.0,<1.26.0',  # 1.22.0 is first to support python 3.11; post 1.26.0 there's issues with scipy
         'pandas>=1.4.2', # first to support 3.11
-        'gensim>=4.3.0', # first to support 3.11
-        'spacy>=3.1.0',
-        'scipy~=1.9.2', # first to support 3.11
-        'transformers>=4.34.0',
+        'gensim>=4.3.0,<5.0.0',  # 5.3.0 is first to support 3.11; avoid major version bump
+        'spacy>=3.6.0,<4.0.0',  # Some later model packs (e.g HPO) are made with 3.6.0 spacy model; avoid major version bump
+        'scipy~=1.9.2',  # 1.9.2 is first to support 3.11
+        'transformers>=4.34.0,<5.0.0',  # avoid major version bump
         'accelerate>=0.23.0', # required by Trainer class in de-id
-        'torch>=1.13.0', # first to support 3.11
+        'torch>=1.13.0,<3.0.0', # 1.13 is first to support 3.11; 2.1.2 has been compatible, but avoid major 3.0.0 for now
         'tqdm>=4.27',
-        'scikit-learn>=1.1.3', # first to supporrt 3.11
-        'dill>=0.3.4', # allow later versions with later versions of datasets (tested with 0.3.6)
-        'datasets>=2.2.2', # allow later versions, tested with 2.7.1
+        'scikit-learn>=1.1.3,<2.0.0',  # 1.1.3 is first to supporrt 3.11; avoid major version bump
+        'dill>=0.3.6,<1.0.0', # stuff saved in 0.3.6/0.3.7 is not always compatible with 0.3.4/0.3.5; avoid major bump
+        'datasets>=2.2.2,<3.0.0', # avoid major bump
         'jsonpickle>=2.0.0', # allow later versions, tested with 3.0.0
         'psutil>=5.8.0',
         # 0.70.12 uses older version of dill (i.e less than 0.3.5) which is required for datasets
