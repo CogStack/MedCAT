f4ef235495b69648bd68fee05f52ffdf5b548b30
mart-r <mart.ratas@gmail.com>
2024-02-16 16:04:58 +0000

    Bump pydantic dependency version

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ setuptools.setup(
         'xxhash>=3.0.0', # allow later versions, tested with 3.1.0
         'blis>=0.7.5', # allow later versions, tested with 0.7.9
         'click>=8.0.4', # allow later versions, tested with 8.1.3
-        'pydantic>=1.10.0,<2.0', # for spacy compatibility; avoid 2.0 due to breaking changes
+        'pydantic>=2.6.0,<3.0', # avoid next major
         ],
     classifiers=[
         "Programming Language :: Python :: 3",
