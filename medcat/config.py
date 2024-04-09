9c69aa9bb0e7fdfab5f206d0be2425f21ee64684
Mart Ratas <mart.ratas@gmail.com>
2024-04-09 09:42:54 +0100

    CU-86947ja9y dill old weights (#411)
    * CU-86947ja9y: Add explicit temporary file removal
    
    * CU-86947ja9y: Add test for weighter average function as loaded from example model pack
    
    * CU-86947ja9y: Modify test for weighted average function as loaded from example CDB
    
    * CU-86947ja9y: Add new remnants of known broken CDB
    
    * CU-86947ja9y: Add fixed remnants of known broken CDB
    
    * CU-86947ja9y: Fix issue with loading partial methods saved in old python version (config.linking.weighted_average_function)
    
    * CU-86947ja9y: Move the fix to a separate utils module along with the default weighted average function (to avoid circular imports)

--- medcat/config.py
+++ medcat/config.py
@@ -10,13 +10,11 @@ import re
 
 from medcat.utils.hasher import Hasher
 from medcat.utils.matutils import intersect_nonempty_set
+from medcat.utils.config_utils import attempt_fix_weighted_average_function
+from medcat.utils.config_utils import weighted_average
 
 
 logger = logging.getLogger(__name__)
-
-
-def weighted_average(step: int, factor: float) -> float:
-    return max(0.1, 1 - (step ** 2 * factor))
 
 
 def workers(workers_override: Optional[int] = None) -> int:
@@ -32,6 +30,10 @@ class FakeDict:
         except AttributeError as e:
             raise KeyError from e
 
+    def __setattr__(self, arg: str, val) -> None:
+        if isinstance(self, Linking) and arg == "weighted_average_function":
+            val = attempt_fix_weighted_average_function(val)
+        super().__setattr__(arg, val)
 
     def __setitem__(self, arg: str, val) -> None:
         setattr(self, arg, val)
