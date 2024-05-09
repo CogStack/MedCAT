c0ba2b95eae1406f3abd85a10b5c1b4eb1a8afa9
mart-r <mart.ratas@gmail.com>
2024-05-09 11:06:55 +0100

    CU-8694gzbud: Add doc strings to methods

--- medcat/utils/cdb_state.py
+++ medcat/utils/cdb_state.py
@@ -24,15 +24,50 @@ CDBState = TypedDict(
         'name_isupper': Dict,
         'vocab': Dict[str, int],
     })
+"""CDB State.
+
+This is a dictionary of the parts of the CDB that change during
+(supervised) training. It can be used to store and restore the
+state of a CDB after modifying it.
+
+Currently, the following fields are saved:
+ - name2cuis
+ - snames
+ - cui2names
+ - cui2snames
+ - cui2context_vectors
+ - cui2count_train
+ - name_isupper
+ - vocab
+"""
 
 
 def copy_cdb_state(cdb) -> CDBState:
+    """Creates a (deep) copy of the CDB state.
+
+    Grabs the fields that correspond to the state,
+    creates deep copies, and returns the copies.
+
+    Args:
+        cdb: The CDB from which to grab the state.
+
+    Returns:
+        CDBState: The copied state.
+    """
     return cast(CDBState, {
         k: deepcopy(getattr(cdb, k)) for k in CDBState.__annotations__
     })
 
 
 def save_cdb_state(cdb, file_path: str) -> None:
+    """Saves CDB state in a file.
+
+    Currently uses `dill.dump` to save the relevant fields/values.
+
+    Args:
+        cdb: The CDB from which to grab the state.
+        file_path (str): The file to dump the state.
+    """
     # NOTE: The difference is that we don't create a copy here.
     #       That is so that we don't have to occupy the memory for
     #       both copies
@@ -45,11 +80,31 @@ def save_cdb_state(cdb, file_path: str) -> None:
 
 
 def apply_cdb_state(cdb, state: CDBState) -> None:
+    """Apply the specified state to the specified CDB.
+
+    This overwrites the current state of the CDB with one provided.
+
+    Args:
+        cdb: The CDB to apply the state to.
+        state (CDBState): The state to use.
+    """
     for k, v in state.items():
         setattr(cdb, k, v)
 
 
 def load_and_apply_cdb_state(cdb, file_path: str) -> None:
+    """Delete current CDB state and apply CDB state from file.
+
+    This first delets the current state of the CDB.
+    This is to save memory. The idea is that saving the staet
+    on disk will save on RAM usage. But it wouldn't really
+    work too well if upon load, two instances were still in
+    memory.
+
+    Args:
+        cdb: The CDB to apply the state to.
+        file_path (str): The file where the state has been saved to.
+    """
     # clear existing data on CDB
     # this is so that we don't occupy the memory for both the loaded
     # and the on-CDB data
@@ -67,6 +122,24 @@ def load_and_apply_cdb_state(cdb, file_path: str) -> None:
 
 @contextlib.contextmanager
 def captured_state_cdb(cdb, save_state_to_disk: bool = False):
+    """A context manager that captures and re-applies the initial CDB state.
+
+    The context manager captures/copies the initial state of the CDB when entering.
+    It then allows the user to modify the state (i.e training).
+    Upon exit re-applies the initial CDB state.
+
+    If RAM is an issue, it is recommended to use `save_state_to_disk`.
+    Otherwise the copy of the original state will be held in memory.
+    If saved on disk, a temporary file is used and removed afterwards.
+
+    Args:
+        cdb: The CDB to use.
+        save_state_to_disk (bool): Whether to save state on disk or hold in in memory.
+            Defaults to False.
+
+    Yields:
+        None
+    """
     if save_state_to_disk:
         with on_disk_memory_capture(cdb):
             yield
@@ -77,6 +150,14 @@ def captured_state_cdb(cdb, save_state_to_disk: bool = False):
 
 @contextlib.contextmanager
 def in_memory_state_capture(cdb):
+    """Capture the CDB state in memory.
+
+    Args:
+        cdb: The CDB to use.
+
+    Yields:
+        None
+    """
     state = copy_cdb_state(cdb)
     yield
     apply_cdb_state(cdb, state)
@@ -84,6 +165,14 @@ def in_memory_state_capture(cdb):
 
 @contextlib.contextmanager
 def on_disk_memory_capture(cdb):
+    """Capture the CDB state in a temporary file.
+
+    Args:
+        cdb: The CDB to use
+
+    Yields:
+        None
+    """
     with tempfile.NamedTemporaryFile() as tf:
         save_cdb_state(cdb, tf.name)
         yield
