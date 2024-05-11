c133a91aca8d121dcfd296bbf11d7c00c7c91fe6
mart-r <mart.ratas@gmail.com>
2024-05-11 13:09:47 +0100

    Add 5 more minutes to delay in tests

--- tests/test_delay.py
+++ tests/test_delay.py
@@ -5,5 +5,5 @@ import unittest
 class DelayTest(unittest.TestCase):
     def test_delay(self):
         print("Sleeping...")
-        time.sleep(3*60) # 3 minutes
+        time.sleep(8*60) # 8 minutes
         print("Done sleeping")
