f48e08402d92801f63d0e00e789aa63eea5b7c58
mart-r <mart.ratas@gmail.com>
2024-05-16 15:59:47 +0100

    CU-8693n892x: Add doc strings

--- medcat/utils/saving/envsnapshot.py
+++ medcat/utils/saving/envsnapshot.py
@@ -5,6 +5,11 @@ import platform
 
 
 def get_installed_packages() -> List[List[str]]:
+    """Get the installed packages and their versions.
+
+    Returns:
+        List[List[str]]: List of lists. Each item contains of a dependency name and version.
+    """
     installed_packages = []
     for package in pkg_resources.working_set:
         installed_packages.append([package.project_name, package.version])
@@ -12,6 +17,13 @@ def get_installed_packages() -> List[List[str]]:
 
 
 def get_environment_info() -> Dict[str, Any]:
+    """Get the current environment information.
+
+    This includes dependency versions, the OS, the CPU architecture and the python version.
+
+    Returns:
+        Dict[str, Any]: _description_
+    """
     return {
         "dependencies": get_installed_packages(),
         "os": platform.platform(),
