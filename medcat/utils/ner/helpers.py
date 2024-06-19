481abb1d82f44ea7cadd8deb789a96dd09309ce1
mart-r <mart.ratas@gmail.com>
2024-06-19 11:42:10 +0100

    CU-8694vte2g: Remove medcat.utils.ner.helper.deid_text method

--- medcat/utils/ner/helpers.py
+++ medcat/utils/ner/helpers.py
@@ -3,35 +3,6 @@ from typing import Callable, Dict, Optional
 from medcat.utils.data_utils import count_annotations
 from medcat.cdb import CDB
 
-from medcat.utils.decorators import deprecated
-
-
-# For now, we will keep this method separate from the above class
-# This is so that we wouldn't need to create a thorwaway object
-# when calling the method from .helpers where it used to be.
-# After the deprecated method in .helpers is removed, we can
-# move this to a proper class method.
-def _deid_text(cat, text: str, redact: bool = False) -> str:
-    """De-identify text.
-
-    De-identified text.
-    If redaction is enabled, identifiable entities will be
-    replaced with starts (e.g `*****`).
-    Otherwise, the replacement will be the CUI or in other words,
-    the type of information that was hidden (e.g [PATIENT]).
-
-
-    Args:
-        cat (CAT): The CAT object to use for deid.
-        text (str): The input document.
-        redact (bool): Whether to redact. Defaults to False.
-
-    Returns:
-        str: The de-identified document.
-    """
-    entities = cat.get_entities(text)['entities']
-    return replace_entities_in_text(text, entities, cat.cdb.get_name, redact=redact)
-
 
 def replace_entities_in_text(text: str,
                              entities: Dict,
@@ -43,14 +14,6 @@ def replace_entities_in_text(text: str,
                  ) if redact else get_cui_name(ent['cui'])
         new_text = new_text[:ent['start']] + f'[{r}]' + new_text[ent['end']:]
     return new_text
-
-
-@deprecated("API now allows creating a DeId model (medcat.utils.ner.deid.DeIdModel). "
-            "It aims to simplify the usage of DeId models. "
-            "The use of this model is encouraged over the use of this method.",
-            depr_version=(1, 8, 0), removal_version=(1, 12, 0))
-def deid_text(*args, **kwargs) -> str:
-    return _deid_text(*args, **kwargs)
 
 
 def make_or_update_cdb(json_path: str, cdb: Optional[CDB] = None,
