6fb68c2315dc644a90474cc9074625463659f043
Mart Ratas <mart.ratas@gmail.com>
2024-08-12 16:34:50 +0100

    CU-8694fwyje: Update all configs with pre-load parts documented (#473)

--- medcat/config_meta_cat.py
+++ medcat/config_meta_cat.py
@@ -17,10 +17,16 @@ class General(MixingConfig, BaseModel):
 
     If set to True, the component lock is not used."""
     seed: int = 13
+    """The seed for random number generation.
+
+    NOTE: If used along RelCAT or additional NER, only one of the seeds will take effect
+    NB! For these changes to take effect, the pipe would need to be recreated."""
     description: str = "No description"
     """Should provide a basic description of this MetaCAT model"""
     category_name: Optional[str] = None
-    """What category is this meta_cat model predicting/training"""
+    """What category is this meta_cat model predicting/training.
+
+    NB! For these changes to take effect, the pipe would need to be recreated."""
     category_value2id: Dict = {}
     """Map from category values to ID, if empty it will be autocalculated during training"""
     vocab_size: Optional[int] = None
@@ -44,6 +50,8 @@ class General(MixingConfig, BaseModel):
     Choose from:
         - 'bbpe': Byte Pair Encoding Tokenizer
         - 'bert-tokenizer': BERT Tokenizer
+
+    NB! For these changes to take effect, the pipe would need to be recreated.
     """
     save_and_reuse_tokens: bool = False
     """This is a dangerous option, if not sure ALWAYS set to False. If set, it will try to share the pre-calculated
@@ -73,12 +81,15 @@ class Model(MixingConfig, BaseModel):
 
     Note:
         When changing the model, make sure to change the tokenizer accordingly.
+        NB! For these changes to take effect, the pipe would need to be recreated.
     """
     model_variant: str = 'bert-base-uncased'
     """
     Applicable only when using BERT:
 
     Specifies the model variant to be used.
+
+    NB! For these changes to take effect, the pipe would need to be recreated.
     """
     model_freeze_layers: bool = True
     """
@@ -88,18 +99,29 @@ class Model(MixingConfig, BaseModel):
 
     - If True: BERT layers are frozen and only the fully connected (FC) layer(s) on top are trained.
     - If False: Parameter-efficient fine-tuning will be applied using Low-Rank Adaptation (LoRA).
+
+    NB! For these changes to take effect, the pipe would need to be recreated.
     """
     num_layers: int = 2
-    """Number of layers in the model (both LSTM and BERT)"""
+    """Number of layers in the model (both LSTM and BERT)
+
+    NB! For these changes to take effect, the pipe would need to be recreated."""
     input_size: int = 300
     """
     Specifies the size of the embedding layer.
 
     Applicable only for LSTM model and ignored for BERT as BERT's embedding size is predefined.
+
+    NB! For these changes to take effect, the pipe would need to be recreated.
     """
     hidden_size: int = 300
-    """Number of neurons in the hidden layer"""
+    """Number of neurons in the hidden layer.
+
+    NB! For these changes to take effect, the pipe would need to be recreated."""
     dropout: float = 0.5
+    """The dropout for the model.
+
+    NB! For these changes to take effect, the pipe would need to be recreated."""
     phase_number: int = 0
     """Indicates whether two phase learning is to be used for training.
 
@@ -119,19 +141,30 @@ class Model(MixingConfig, BaseModel):
 
     If fc2 is True and fc3 is set to True, then the 3rd fully connected layer is used
 
-    If lr_scheduler is set to True, then the learning rate scheduler is used with the optimizer    
+    If lr_scheduler is set to True, then the learning rate scheduler is used with the optimizer
+
+    NB! For these changes to take effect, the pipe would need to be recreated.
     """
     num_directions: int = 2
     """Applicable only for LSTM:
 
-    2 - bidirectional model, 1 - unidirectional"""
+    2 - bidirectional model, 1 - unidirectional
+
+    NB! For these changes to take effect, the pipe would need to be recreated."""
     nclasses: int = 2
-    """Number of classes that this model will output"""
+    """Number of classes that this model will output.
+
+    NB! For these changes to take effect, the pipe would need to be recreated."""
     padding_idx: int = -1
+    """The padding index.
+
+    NB! For these changes to take effect, the pipe would need to be recreated."""
     emb_grad: bool = True
     """Applicable only for LSTM:
 
-    If True, the embeddings will also be trained"""
+    If True, the embeddings will also be trained.
+
+    NB! For these changes to take effect, the pipe would need to be recreated."""
     ignore_cpos: bool = False
     """If set to True center positions will be ignored when calculating representation"""
 
