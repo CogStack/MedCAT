fadc7d18e695e8217e26e5191ff11e36dac44fde
mart-r <mart.ratas@gmail.com>
2024-08-13 14:14:39 +0100

    CU-8695d4www: Fix allowing deprecation during test time

--- tests/__init__.py
+++ tests/__init__.py
@@ -14,11 +14,11 @@ class DeprecatedMethodCallException(ValueError):
 
 
 def deprecation_exception_raiser(message: str, depr_version: Tuple[int, int, int],
-                     removal_version: Tuple[int, int, int]):
+                     removal_version: Tuple[int, int, int],
+                     allow_usage: bool = False):
     def decorator(func: Callable) -> Callable:
         def wrapper(*args, **kwargs):
-            if ('allow_usage' in kwargs and kwargs['allow_usage'] or
-                    len(args) >= 4 and args[3]):
+            if allow_usage:
                 return func(*args, **kwargs)
             raise DeprecatedMethodCallException(func, message, depr_version, removal_version)
         return wrapper
