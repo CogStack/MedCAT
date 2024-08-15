6c76acc8a334ee5cc1fc2bf10a83db9aeb84ca87
mart-r <mart.ratas@gmail.com>
2024-08-15 10:13:44 +0100

    Revert "CU-8695d4www: Allow usage of pydantic 1-2 workaround methods during tests"
    This reverts commit 825628e10db04b45143e8ec84af71781a04d7725.

--- medcat/utils/pydantic_version.py
+++ medcat/utils/pydantic_version.py
@@ -8,8 +8,7 @@ HAS_PYDANTIC2 = pydantic_version.startswith("2.")
 
 
 @deprecated("At some point we will move away from supporting pydantic 1",
-            depr_version=(1, 13, 0), removal_version=(1, 15, 0),
-            allow_usage=True)
+            depr_version=(1, 13, 0), removal_version=(1, 15, 0))
 def get_model_dump(obj: BaseModel) -> dict:
     """Helper method to get the model dump of a pydnatic model.
 
@@ -29,8 +28,7 @@ def get_model_dump(obj: BaseModel) -> dict:
 
 
 @deprecated("At some point we will move away from supporting pydantic 1",
-            depr_version=(1, 13, 0), removal_version=(1, 15, 0),
-            allow_usage=True)
+            depr_version=(1, 13, 0), removal_version=(1, 15, 0))
 def get_model_fields(obj: BaseModel) -> dict:
     if HAS_PYDANTIC2:
         return obj.model_fields
