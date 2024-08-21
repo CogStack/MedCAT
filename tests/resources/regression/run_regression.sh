101f2e476c31631ce55cf079b7c0e8479ba23d54
mart-r <mart.ratas@gmail.com>
2024-08-21 17:39:38 +0100

    CU-86956du3q: Update comments in regression bash script

--- tests/resources/regression/run_regression.sh
+++ tests/resources/regression/run_regression.sh
@@ -4,6 +4,8 @@
 set -e
 
 # create and train model and capture output
+# this will create a model pack based on some data included within the tests/resources/regression/creation/ folder,
+# it will then train on some self-supervised as well as supervised training data and save the model.
 output=$(python tests/resources/regression/creation/cat_creation.py)
 # make sure the user sees the output
 echo "$output"
@@ -13,6 +15,7 @@ model_path=$(echo "$output" | tail -n 1)
 # NOTE: this file should be tagged with the python version we're using
 
 # run the regression_checker with the captured file path
+# if any of the regression cases fail, this will return a non-zero exit status
 python -m medcat.utils.regression.regression_checker \
   "$model_path" \
   tests/resources/regression/testing/test_model_regresssion.yml \
