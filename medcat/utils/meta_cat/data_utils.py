3be0dc1462902637612d84ae4dbc68302ccf8f13
shubham-s-agarwal <66172189+shubham-s-agarwal@users.noreply.github.com>
2024-10-09 15:37:22 +0100

    Pushing change

--- medcat/utils/meta_cat/data_utils.py
+++ medcat/utils/meta_cat/data_utils.py
@@ -179,6 +179,11 @@ def encode_category_values(data: Dict, existing_category_value2id: Optional[Dict
     else:
         category_value2id = {}
 
+    category_values = set([x[2] for x in data])
+    for c in category_values:
+        if c not in category_value2id:
+            category_value2id[c] = len(category_value2id)
+
     # Map values to numbers
     for i in range(len(data)):
         data[i][2] = category_value2id[data[i][2]]
