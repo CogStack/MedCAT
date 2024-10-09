210741c4298830713e19c19d804cfbda966253f6
shubham-s-agarwal <66172189+shubham-s-agarwal@users.noreply.github.com>
2024-10-09 12:01:31 +0100

    Pushing type fix

--- medcat/utils/meta_cat/ml_utils.py
+++ medcat/utils/meta_cat/ml_utils.py
@@ -421,8 +421,8 @@ def eval_model(model: nn.Module, data: List, config: ConfigMetaCAT, tokenizer: T
         )
     except ValueError as e:
         logger.warning("The evaluation dataset does not contain all the labels, some labels are missing. Performance displayed for labels found...")
-        labels_present = set(predictions)
-        labels_present = [str(x) for x in labels_present]
+        labels_present_: set = set(predictions)
+        labels_present: list[str] = [str(x) for x in labels_present_]
 
         confusion = pd.DataFrame(
             data=confusion_matrix(y_eval, predictions, ),
