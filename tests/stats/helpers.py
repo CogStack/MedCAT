11b1c7a20bf373ddf7f18d4ec790bb54d082232f
mart-r <mart.ratas@gmail.com>
2024-10-15 15:34:41 +0100

    CU-869671bn4: Fix stats helper method - use correct type adapter

--- tests/stats/helpers.py
+++ tests/stats/helpers.py
@@ -4,13 +4,9 @@ from unittest import TestCase
 import pydantic.error_wrappers
 
 from medcat.stats.mctexport import MedCATTrainerExport
-from medcat.utils.pydantic_version import HAS_PYDANTIC2
 
 
-if HAS_PYDANTIC2:
-    MCTExportPydanticModel = pydantic.TypeAdapter(MedCATTrainerExport)
-else:
-    MCTExportPydanticModel = pydantic.create_model_from_typeddict(MedCATTrainerExport)
+MCTExportPydanticModel = pydantic.TypeAdapter(MedCATTrainerExport)
 
 
 
@@ -26,12 +22,5 @@ def nullify_doc_names_proj_ids(export: MedCATTrainerExport) -> MedCATTrainerExpo
 
 
 def assert_is_mct_export(tc: TestCase, mct_export: dict):
-    if HAS_PYDANTIC2:
-        model_instance = MCTExportPydanticModel.validate_python(mct_export)
-        tc.assertIsInstance(model_instance, dict)  # NOTE: otherwise would have raised an exception
-    else:
-        try:
-            model = MCTExportPydanticModel(**mct_export)
-        except pydantic.error_wrappers.PydanticValidationError as e:
-            raise AssertionError("Not an MCT export") from e
-        tc.assertIsInstance(model, MCTExportPydanticModel)
+    model_instance = MCTExportPydanticModel.validate_python(mct_export)
+    tc.assertIsInstance(model_instance, dict)  # NOTE: otherwise would have raised an exception
