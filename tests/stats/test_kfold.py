00c0dd0d9b1d4b8786a0f9927f8e57e060f15bb0
Mart Ratas <mart.ratas@gmail.com>
2024-12-09 12:40:47 +0000

    CU-8696v2j42: Add test to make sure per-cui counts are kept when creating folds (#508)
    * CU-8696v2j42: Add test to make sure per-cui counts are kept when creating folds
    
    * CU-8696v2j42: Fix per annotation fold creation

--- tests/stats/test_kfold.py
+++ tests/stats/test_kfold.py
@@ -2,6 +2,7 @@ import os
 import json
 from typing import Dict, Union, Optional
 from copy import deepcopy
+from collections import Counter
 
 from medcat.stats import kfold
 from medcat.cat import CAT
@@ -79,6 +80,20 @@ class KFoldCreatorTests(MCTExportTests):
             total_anns += anns
         count_all_once = kfold.count_all_annotations(self.mct_export)
         self.assertEqual(total_anns, count_all_once)
+
+    def count_cuis(self, export: MCTExportTests) -> Counter:
+        cntr = Counter()
+        for _, _, ann in kfold.iter_anns(export):
+            cui = ann["cui"]
+            cntr[cui] += 1
+        return cntr
+
+    def test_folds_keep_ann_targets(self):
+        orig_cntr = self.count_cuis(self.mct_export)
+        fold_counter = Counter()
+        for fold in self.folds:
+            fold_counter += self.count_cuis(fold)
+        self.assertEqual(orig_cntr, fold_counter)
 
     def test_1fold_same_as_orig(self):
         folds = kfold.get_fold_creator(self.mct_export, 1, split_type=self.SPLIT_TYPE).create_folds()
