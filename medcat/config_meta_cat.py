f72fc288ede7fb31b833a323bc1d4a7c2b068eae
shubham-s-agarwal <66172189+shubham-s-agarwal@users.noreply.github.com>
2025-01-24 12:27:27 +0000

    Pushing update for MetaCAT
    - Addressing the multiple zero-division-error warnings per epoch while training
    - Accommodating the variations in category name and class name across NHS sites

--- medcat/config_meta_cat.py
+++ medcat/config_meta_cat.py
@@ -1,4 +1,4 @@
-from typing import Dict, Any
+from typing import Dict, Any, List
 from medcat.config import MixingConfig, BaseModel, Optional
 
 
@@ -27,8 +27,22 @@ class General(MixingConfig, BaseModel):
     """What category is this meta_cat model predicting/training.
 
     NB! For these changes to take effect, the pipe would need to be recreated."""
+    category_names_map: List = []
+    """Map that stores the variations of possible category names
+    Example: For Experiencer, the alternate name is Subject
+    category_names_map: ['Experiencer','Subject']
+
+    In the case that one specified in 'category_name' parameter does not match the data, this ensures no error is raised and it is automatically mapped
+    """
+
     category_value2id: Dict = {}
     """Map from category values to ID, if empty it will be autocalculated during training"""
+    class_names_map: List[List] = [[]]
+    """Map that stores the variations of possible class names for the given category (task)
+    Example: For Presence task, the class names vary across NHS sites.
+    To accommodate for this, class_names_map is populated as: [["Hypothetical (N/A)","Hypothetical"],["Not present (False)","False"],["Present (True)","True"]]
+    Each sub list contains the possible variations of the given class.
+    """
     vocab_size: Optional[int] = None
     """Will be set automatically if the tokenizer is provided during meta_cat init"""
     lowercase: bool = True
