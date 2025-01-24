c7030145187e28291091192a5d91aba5f13d2297
shubham-s-agarwal <66172189+shubham-s-agarwal@users.noreply.github.com>
2025-01-24 16:06:05 +0000

    Pushing type fix

--- medcat/meta_cat.py
+++ medcat/meta_cat.py
@@ -265,12 +265,12 @@ class MetaCAT(PipeRunner):
         if not category_value2id:
             # Encode the category values
             full_data, data_undersampled, category_value2id = encode_category_values(data,
-                                                                                     category_undersample=self.config.model.category_undersample,class_name_map=g_config['alternative_class_names'])
+                                                                                     category_undersample=self.config.model.category_undersample,alternative_class_names=g_config['alternative_class_names'])
         else:
             # We already have everything, just get the data
             full_data, data_undersampled, category_value2id = encode_category_values(data,
                                                                                      existing_category_value2id=category_value2id,
-                                                                                     category_undersample=self.config.model.category_undersample,class_name_map=g_config['alternative_class_names'])
+                                                                                     category_undersample=self.config.model.category_undersample,alternative_class_names=g_config['alternative_class_names'])
         g_config['category_value2id'] = category_value2id
         self.config.model['nclasses'] = len(category_value2id)
 
