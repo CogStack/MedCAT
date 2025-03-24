e8e6c5890fcaa8f062c2e57086b7f633012589e4
Mart Ratas <mart.ratas@gmail.com>
2025-03-24 13:36:31 +0200

    CU-8698ek477: Fix AdamW import from tranformers to torch (#523)
    * CU-8698ek477: Add TODO to MetaCAT ML utils regarding AdamW import
    
    * CU-8698ek477: Fix AdamW import (trf->torch)

--- medcat/utils/meta_cat/ml_utils.py
+++ medcat/utils/meta_cat/ml_utils.py
@@ -14,7 +14,8 @@ from medcat.tokenizers.meta_cat_tokenizers import TokenizerWrapperBase
 from sklearn.metrics import classification_report, precision_recall_fscore_support, confusion_matrix
 from sklearn.model_selection import train_test_split
 from sklearn.utils.class_weight import compute_class_weight
-from transformers import AdamW, get_linear_schedule_with_warmup
+from transformers import get_linear_schedule_with_warmup
+from torch.optim import AdamW
 
 
 import logging
