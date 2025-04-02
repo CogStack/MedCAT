255334da7a6e9a4ad075b4e6b05292022a68d5c4
mart-r <mart.ratas@gmail.com>
2025-04-02 12:52:00 +0100

    Update base relcat model implementation to include config

--- medcat/utils/relation_extraction/models.py
+++ medcat/utils/relation_extraction/models.py
@@ -4,7 +4,10 @@ from transformers.models.bert.modeling_bert import BertModel
 from transformers import ModernBertModel
 from transformers.models.llama import LlamaModel
 
+from medcat.config_rel_cat import ConfigRelCAT
+
 
 class Base_RelationExtraction(nn.Module):
 
     hf_model: Union[BertModel, ModernBertModel, LlamaModel]
+    relcat_config: ConfigRelCAT
